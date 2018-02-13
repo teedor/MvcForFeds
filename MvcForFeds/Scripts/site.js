68cdc8d61b6df617aa0bbed467333cafd7f49758
Stephen Anderson <stephen.anderson@madetoengage.com>
2018-02-13 14:29:39 +0000

    added jquery ui

---
+++ MvcForFeds/Scripts/site.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    $(".datePicker").datepicker({
+        showOn: "button",
+        buttonImage: "/images/calendar-icon.png",
+        buttonImageOnly: true,
+        buttonText: "Select date"
+    });
+});
